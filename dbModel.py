1fe32e650a2c4c076a84de0e6201ad541676d800
KovalevAnd <KovalevAnd@yandex.ru>
2022-01-17 19:14:39 +0300

    переделал для проверки postman

--- dbModel.py
+++ dbModel.py
@@ -2,6 +2,7 @@ import json
 
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 # использовал один раз при заполнении таблиц, возможно код с созданием таблиц надо вынести в отдельный файл
 # from func import read_json
 
@@ -106,55 +107,55 @@ def page_users():
                     "role": user.role,
                     "phone": user.phone
                 })
-        return render_template('users_id.html', json=json.dumps(user_response))
-    elif request.method == 'POST' and not request.form.get('del') and not request.form.get('upd_id'):
+        return json.dumps(user_response)
+    elif request.method == 'POST':
+        user_data = json.loads(request.data)
         new_user = User(
-            first_name=request.form.get('first_name'),
-            last_name=request.form.get('last_name'),
-            age=request.form.get('age'),
-            email=request.form.get('email'),
-            role=request.form.get('role'),
-            phone=request.form.get('phone')
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            age=user_data['age'],
+            email=user_data['email'],
+            role=user_data['role'],
+            phone=user_data['phone']
         )
         db.session.add(new_user)
         db.session.commit()
-        return 'пользователь добавлен', 201
-    elif request.method == 'POST' and request.form.get('del'):
-        sid = int(request.form.get('del'))
-        user = User.query.get(sid)
-        db.session.delete(user)
-        db.session.commit()
-        return f'пользователь {sid} удален', 201
-    elif request.method == 'POST' and request.form.get('upd_id'):
-        upd_id = int(request.form.get('upd_id'))
-        upd_user = User.query.get(upd_id)
-        upd_user.first_name = request.form.get('first_name')
-        upd_user.last_name = request.form.get('last_name')
-        upd_user.age = request.form.get('age')
-        upd_user.email = request.form.get('email')
-        upd_user.role = request.form.get('role')
-        upd_user.phone = request.form.get('phone')
-
-        db.session.add(upd_user)
-        db.session.commit()
-        return 'пользователь обновлен', 201
+        return 'new user created', 201
 
 
-@app.route('/users/<int:sid>', methods=['GET'])
+@app.route('/users/<int:sid>', methods=['GET', 'PUT', 'DELETE'])
 def page_users_sid(sid: int):
-    user = User.query.get(sid)
-    if user is None:
-        return "user not found"
+    if request.method == 'GET':
+        user = User.query.get(sid)
+        if user is None:
+            return "user not found"
 
-    return json.dumps({
-        "id": user.id,
-        "first_name": user.first_name,
-        "last_name": user.last_name,
-        "age": user.age,
-        "email": user.email,
-        "role": user.role,
-        "phone": user.phone
-    })
+        return json.dumps({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "age": user.age,
+            "email": user.email,
+            "role": user.role,
+            "phone": user.phone
+        })
+    elif request.method == 'DELETE':
+        u = User.query.get(sid)
+        db.session.delete(u)
+        db.session.commit()
+        return f'user {sid} deleted', 204
+    elif request.method == 'PUT':
+        user_data = json.loads(request.data)
+        u = User.query.get(sid)
+        u.first_name = user_data['first_name']
+        u.last_name = user_data['last_name']
+        u.age = user_data['age']
+        u.email = user_data['email']
+        u.role = user_data['role']
+        u.phone = user_data['phone']
+        db.session.add(u)
+        db.session.commit()
+        return f'user {sid} updated', 204
 
 
 @app.route('/orders/', methods=['GET', 'POST'])
@@ -175,64 +176,61 @@ def page_orders():
                     "customer_id": order.customer_id,
                     "executor_id": order.executor_id
                 })
-        return render_template('orders_id.html', json=order_response)
-    if request.method == 'POST' and not request.form.get('del_ord') and not request.form.get('upd_ord_id'):
+        return jsonify(order_response)
+    if request.method == 'POST':
+        user_data = json.loads(request.data)
         new_order = Order(
-            name=request.form.get('name'),
-            description=request.form.get('description'),
-            start_date=request.form.get('start_date'),
-            end_date=request.form.get('end_date'),
-            address=request.form.get('address'),
-            price=request.form.get('price'),
-            customer_id=request.form.get('customer_id'),
-            executor_id=request.form.get('executor_id')
+            name=user_data["name"],
+            description=user_data["description"],
+            start_date=user_data["start_date"],
+            end_date=user_data["end_date"],
+            address=user_data["address"],
+            price=user_data["price"],
+            customer_id=user_data["customer_id"],
+            executor_id=user_data["executor_id"]
         )
         db.session.add(new_order)
         db.session.commit()
-        return 'Ордер добавлен', 201
-    if request.method == 'POST' and request.form.get('del_ord'):
-        sid = int(request.form.get('del_ord'))
-        order = Order.query.get(sid)
-        db.session.delete(order)
-        db.session.commit()
-        return f'Ордер {sid} удален', 201
-    if request.method == 'POST' and request.form.get('upd_ord_id'):
-        upd_ord_id = int(request.form.get('upd_ord_id'))
-        upd_ord = Order.query.get(upd_ord_id)
-        upd_ord.name = request.form.get('name')
-        upd_ord.description = request.form.get('description')
-        upd_ord.start_date = request.form.get('start_date')
-        upd_ord.end_date = request.form.get('end_date')
-        upd_ord.address = request.form.get('address')
-        upd_ord.price = request.form.get('price')
-        upd_ord.customer_id = request.form.get('customer_id')
-        upd_ord.executor_id = request.form.get('executor_id')
+        return 'new order created', 201
 
 
-        db.session.add(upd_ord)
-        db.session.commit()
-        return 'Ордер обновлен', 201
-
-
-
-
-@app.route('/orders/<int:sid>', methods=['GET'])
+@app.route('/orders/<int:sid>', methods=['GET', 'PUT', 'DELETE'])
 def page_order_sid(sid: int):
-    order = Order.query.get(sid)
-    if order is None:
-        return "order not found"
-    json_raw = {
-        "id": order.id,
-        "name": order.name,
-        "description": order.description,
-        "start_date": order.start_date,
-        "end_date": order.end_date,
-        "address": order.address,
-        "price": order.price,
-        "customer_is": order.customer_id,
-        "executor_id": order.executor_id
-    }
-    return render_template('order.html', json=json_raw)
+    if request.method == 'GET':
+        order = Order.query.get(sid)
+        if order is None:
+            return "order not found"
+        json_raw = {
+            "id": order.id,
+            "name": order.name,
+            "description": order.description,
+            "start_date": order.start_date,
+            "end_date": order.end_date,
+            "address": order.address,
+            "price": order.price,
+            "customer_id": order.customer_id,
+            "executor_id": order.executor_id
+        }
+        return json_raw
+    elif request.method == 'DELETE':
+        o = Order.query.get(sid)
+        db.session.delete(o)
+        db.session.commit()
+        return f'order {sid} deleted', 204
+    elif request.method == 'PUT':
+        user_data = json.loads(request.data)
+        o = Order.query.get(sid)
+        o.name = user_data['name']
+        o.description = user_data['description']
+        o.start_date = user_data['start_date']
+        o.end_date = user_data['end_date']
+        o.address = user_data['address']
+        o.price = user_data['price']
+        o.customer_id = user_data['customer_id']
+        o.executor_id = user_data['executor_id']
+        db.session.add(o)
+        db.session.commit()
+        return f'order {sid} updated', 204
 
 
 @app.route('/offers/', methods=['GET', 'POST'])
@@ -247,44 +245,44 @@ def page_offers():
                     "order_id": offer.order_id,
                     "executor_id": offer.executor_id
                 })
-        return render_template('offers_id.html', json= json.dumps(offer_response))
-    elif request.method == 'POST' and not request.form.get('del_offer_id') and not request.form.get('upd_offer_id'):
+        return json.dumps(offer_response)
+    elif request.method == 'POST':
+        user_data = json.loads(request.data)
         new_offer = Offer(
-            order_id=request.form.get('order_id'),
-            executor_id=request.form.get('executor_id')
+            order_id=user_data['order_id'],
+            executor_id=user_data['executor_id']
         )
         db.session.add(new_offer)
         db.session.commit()
         return 'Предложение добавлено', 201
-    elif request.method == 'POST' and request.form.get('del_offer_id'):
-        sid = int(request.form.get('del_offer_id'))
-        offer = Offer.query.get(sid)
-        db.session.delete(offer)
-        db.session.commit()
-        return f'Предложение {sid} удалено', 201
-    elif request.method == 'POST' and request.form.get('upd_offer_id'):
-        upd_offer_id = int(request.form.get('upd_offer_id'))
-        upd_offer = Offer.query.get(upd_offer_id)
-        upd_offer.order_id = request.form.get('order_id')
-        upd_offer.executor_id = request.form.get('executor_id')
-
-        db.session.add(upd_offer)
-        db.session.commit()
-        return 'Предложение обновлено', 201
 
 
-@app.route('/offers/<int:sid>')
+@app.route('/offers/<int:sid>', methods=['GET', 'PUT', 'DELETE'])
 def page_offer_sid(sid: int):
-    offer = Offer.query.get(sid)
-    if offer is None:
-        return "offer not found"
+    if request.method == 'GET':
+        offer = Offer.query.get(sid)
+        if offer is None:
+            return "offer not found"
 
-    return json.dumps(
-        {
-            "id": offer.id,
-            "order_id": offer.order_id,
-            "executor_id": offer.executor_id
-        })
+        return json.dumps(
+            {
+                "id": offer.id,
+                "order_id": offer.order_id,
+                "executor_id": offer.executor_id
+            })
+    elif request.method == 'DELETE':
+        o = Offer.query.get(sid)
+        db.session.delete(o)
+        db.session.commit()
+        return f'offer {sid} deleted', 204
+    elif request.method == 'PUT':
+        user_data = json.loads(request.data)
+        of = Offer.query.get(int(sid))
+        of.order_id = user_data['order_id']
+        of.executor_id = user_data['executor_id']
+        db.session.add(of)
+        db.session.commit()
+        return f'offer {sid} updated', 204
 
 
 app.run()
